8b5bbbdc4773fcd6f384d5a53f3e934acaaebcb5
Rommans <rkokotko3@gmail.com>
2022-12-18 10:14:02 +0200

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
 import Layout from './components/Layout';
@@ -10,14 +10,14 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App bg-gradient-to-r from-light-black to-dark-black min-h-screen flex flex-col justify-between">
-      <BrowserRouter>
+      <HashRouter basename="/">
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/characters/:id" element={<Characters />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
